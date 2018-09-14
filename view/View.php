2ef0a19a8b30f718482c5363ba4439a028935d47
zohaib <zohaib_b13@hotmail.com>
2018-09-14 21:19:16 +0200

    View is working for htdocs

--- view/View.php
+++ view/View.php
@@ -4,22 +4,22 @@
  * @see http://php-html.net/tutorials/model-view-controller-in-php/ The tutorial code used as basis.
  */
 
-Class View {
-	/** Is used to retrieve the title of the given view page
-	 * @return string View page title.
-	 */
-   // private function getPageTitle();
-	
-	/** Is used to retrieve the page content of the given view page
-	 * @return string View page content.
-	 */
-//	private function getPageContent();
+	Class View {
+		/** Is used to retrieve the title of the given view page
+		 * @return string View page title.
+		 */
+		// private function getPageTitle();
 
-	/** Creates HTML code for the given view page
-	 */
-	public function create() {
-	    include('html/main.html');
-	    <link rel="stylesheet" href="css/main.css">
-		 }
+		/** Is used to retrieve the page content of the given view page
+		 * @return string View page content.
+		 */
+		//	private function getPageContent();
+
+		/** Creates HTML code for the given view page
+		 */
+		public function create() {
+		    include('html/main.html');
+		    echo '<link href="./View/css/main.css" rel="stylesheet" >';
+		}
 	}
 ?>
