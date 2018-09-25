dcd6f56a1054bca6a05fb14c4498e9b4e0f5011d
Morten <mortenbjerke95@gmail.com>
2018-09-25 11:58:59 +0200

    implemeted char limits on registrer.php inputs, dotted and minimum password requirement

--- view/Register.php
+++ view/Register.php
@@ -5,25 +5,25 @@
 <form class="registerForm" method="POST" action="index.php">
 	<div class="input-group">
 		<label>First name</label>
-		<input type="text" name="fname">
+		<input type="text" name="fname" maxlength="30">
 		<div class="input-group">
 		<label>Last name</label>
-		<input type="text" name="lname">
+		<input type="text" name="lname" maxlength="30">
 	<div class="input-group">
 		<label>Username</label>
-		<input type="text" name="user" required>
+		<input type="text" name="user" required maxlength="20">
 	</div>
 	<div class="input-group">
 		<label>Email</label>
-		<input type="text" name="email" required>
+		<input type="text" name="email" required maxlength="33">
 	</div>
 	<div class="input-group">
-		<label>Password</label>
-		<input type="text" name="password_1" id="password_1" required>
+		<label>Password (minimum 6 characters)</label>
+		<input type="password" name="password_1" id="password_1" pattern=".{6,}" maxlength="30">
 	</div>
 	<div class="input-group">
 		<label>Confirm Password</label>
-		<input type="text" name="password_2" id="password_2" required>
+		<input type="password" name="password_2" id="password_2" pattern=".{6,}" maxlength="30">
 	</div>
 	<div class="input-group">
 	<button type="submit" name="reg" class="btn">Register</button>
