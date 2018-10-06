5674330a1342c2f90b9b73393e7ccb177a366c19
Morten <mortenbjerke95@gmail.com>
2018-10-06 13:47:12 +0200

    security on reguser is legit good now

--- controller/Controller.php
+++ controller/Controller.php
@@ -17,7 +17,7 @@ include_once("model/Comment.php");
  * @see http://php-html.net/tutorials/model-view-controller-in-php/ The tutorial code used as basis.
  */
 
-function test_input($data) {
+function sanitize($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
@@ -88,26 +88,34 @@ class Controller {
 		else if (isset($_POST['reg'])) {
 
 			$userErr = "";
-			$reguser = test_input($_POST['user']);
-			$regpw = test_input(password_hash($_POST['password_1'], PASSWORD_DEFAULT));
-			$regmail = test_input($_POST['email']);
-			$regfname = test_input($_POST['fname']);
-			$reglname = test_input($_POST['lname']);
+			$reguser = sanitize($_POST['user']);
+			$regpw = sanitize(password_hash($_POST['password_1'], PASSWORD_DEFAULT));
+			$regmail = sanitize($_POST['email']);
+			$regfname = sanitize($_POST['fname']);
+			$reglname = sanitize($_POST['lname']);
 
-			if (empty($_POST['user'])) {
+			if (empty($_POST['fname'])) {
+				echo $userErr = "First name is required";
+			}
+
+			else if(!preg_match("/^[a-zA-Z -]*$/",$regfname)) {
+					echo $userErr = "ERROR! First name can only contain letters!";
+			}
+
+			else if (empty($_POST['lname'])) {
+				echo $userErr = "Last name is required";
+			}
+
+			else if(!preg_match("/^[a-zA-Z -]*$/",$reglname)) {
+					echo $userErr = "ERROR! Last name can only contain letters!";
+			}
+
+			else if (empty($_POST['user'])) {
 				echo $userErr = "Username is required";
 			}
 
-			else if(!preg_match("/[a-zA-Z1-9_]+/",$reguser)) {
+			else if(!preg_match("/^[a-zA-Z0-9_]*$/", $reguser)) {
 					echo $userErr = "ERROR! Can only contain letters and numbers!";
-			}
-
-			else if (empty($_POST['password_1'])) {
-				echo $userErr = "Password is required";
-			}
-
-			else if(!preg_match("/[A-Za-z0-9_.&%@-]+/",$regpw)) {
-					echo $userErr = "ERROR! Password cannot contain some of these characters!";
 			}
 
 			else if (empty($_POST['email'])) {
@@ -118,24 +126,16 @@ class Controller {
 					echo $userErr = "ERROR! Invalid Email format!";
 			}
 
-			else if (empty($_POST['fname'])) {
-				echo $userErr = "First name is required";
+			else if (empty($_POST['password_1'])) {
+				echo $userErr = "Password is required";
 			}
 
-			else if(!preg_match("/[A-Za-z-]+/",$regfname)) {
-					echo $userErr = "ERROR! First name can only contain letters!";
-			}
-
-			else if (empty($_POST['lname'])) {
-				echo $userErr = "Last name is required";
-			}
-
-			else if(!preg_match("/[A-Za-z-]+/",$reglname)) {
-					echo $userErr = "ERROR! Last name can only contain letters!";
+			else if(!preg_match("/^[a-zA-Z0-9#%&!$?+.,_-]*$/",$regpw)) {
+					echo $userErr = "ERROR! Password cannot contain some of these characters!";
 			}
 
 			else {
-				$newUser = new User(-1,	$reguser = test_input($_POST['user']), $regpw = password_hash($_POST['password_1'], PASSWORD_DEFAULT), $regmail = $_POST['email'], $regfname = $_POST['fname'], $reglname = $_POST['lname'],0,0);
+				$newUser = new User(-1,	$reguser = ($_POST['user']), $regpw = password_hash($_POST['password_1'], PASSWORD_DEFAULT), $regmail = $_POST['email'], $regfname = $_POST['fname'], $reglname = $_POST['lname'],0,0);
 				$this->model->registerUser($newUser);
 		   		header("Refresh:0");
 		   	}
